27fddc7a4c83e4d00ba94207fd09a503ec120b2c
jumpeace <tech.jumpei@gmail.com>
2022-11-30 13:16:09 +0900

    Edit: form-page's background gradation

--- src/pages/base/Form.js
+++ src/pages/base/Form.js
@@ -5,10 +5,10 @@ import Form from '../../components/contents/Form';
 
 const FormPage = () => {
     const { baseClassName } = useOutletContext();
-    
+
     return (
         <main 
-            className={`${baseClassName} bg-gradient-to-bl from-green-100 via-teal-100 to-blue-100`}
+            className={`${baseClassName} bg-gradient-to-bl from-cyan-100 via-sky-100 to-blue-100`}
         >
             <div className="mx-auto my-20 flex justify-center items-center">
                 <div
