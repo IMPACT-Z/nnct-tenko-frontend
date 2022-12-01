e085f79c92354f6a340e231773d420d78e1242fd
jumpeace <tech.jumpei@gmail.com>
2022-12-02 00:30:48 +0900

    Init: WebSocket

---
+++ src/functions/restApi.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import Http from './http'
+
+
+class RestApi extends Http {
+    constructor(path) {
+        super();
+        this.fullPath = `${process.env.REACT_APP_API_PREFIX}/${path}`;
+    }
+
+    async get(params={}) {
+        const uri = `${this.getFullPath(path)}?${new URLSearchParams(params).toString()}`;
+    
+        return await new Promise((resolve, reject) => {
+            axios.get(uri, this.headers)
+            .then(response => {
+                resolve(response);
+            })
+            .catch(error => {
+                reject(error.response);
+            });
+        });
+    };
+    
+    async post(data) {
+        const uri = this.fullPath;
+    
+        return await new Promise((resolve, reject) => {
+            axios.post(uri, data)
+            .then(response => {
+                resolve(response);
+            })
+            .catch(error => {
+                reject(error.response);
+            });
+        });
+    };
+}
+
+
+export default RestApi;
