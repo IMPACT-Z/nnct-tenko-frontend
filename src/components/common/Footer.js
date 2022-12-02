4ff334936cfa448c2b53f29adca12db2da4b7463
jumpeace <tech.jumpei@gmail.com>
2022-12-02 11:28:00 +0900

    Refactor

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,14 +1,12 @@
 const Footer = () => {
-    const context = {
-        copyright: {
-            year: 2022,
-            organization: '衝撃のイナズマZ'
-        }
+    const copyright = {
+        year: 2022,
+        organization: '衝撃のイナズマZ'
     }
 
     return (
         <footer className="fixed w-screen h-12 bottom-0 left-0 bg-slate-100 flex flex-col justify-center items-center">
-            <div className="text-slate-900">© { context.copyright.year }, { context.copyright.organization }</div>
+            <div className="text-slate-900">© { copyright.year }, { copyright.organization }</div>
         </footer>
     );
 }
