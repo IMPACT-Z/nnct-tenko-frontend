eaf4a4febb3989b5fae8e2f7551fa4bf0158d103
BonyChops <contact@bonychops.com>
2022-12-13 11:27:56 +0900

    Fix: merge miss

--- src/components/pages/tenko/Do.js
+++ src/components/pages/tenko/Do.js
@@ -7,21 +7,293 @@ import RestApi from '../../../functions/restApi';
 
 import { AUTH_TYPE } from "../../Base";
 import PageBase from "../Base";
+import { getIdTokenExcludingAuth, logout } from "../../../functions/auth";
 
 
-const cameraSetting = {
-    width: 480,
-    height: 270,
-    format: 'png',
-    interval: 2000,
-}
-const videoConstraints = {
-    width: cameraSetting.width,
-    height: cameraSetting.height,
-    facingMode: 'user',
-}
+const TenkoSession = React.memo(({reflectStatus, killSession, messageHTML}) => {
+    const getCameraSetting = useCallback(() => {
+        return {
+            width: 800,
+            height: 450,
+            format: 'jpeg',
+            interval: 1000,
+        }
+    }, []);
+    const getVideoConstraints = useCallback(() => {
+        return {
+            width: getCameraSetting().width,
+            height: getCameraSetting().height,
+            facingMode: 'user',
+        }
+    }, [getCameraSetting]);
+    const webcamRef = useRef(null);
+    const capture = useCallback(() => {
+        return webcamRef.current?.getScreenshot() ?? null;
+    }, [webcamRef]);
 
-const RollCall = () => {
+    const [socket, setSocket] = useState(null);
+    useEffect(() => {
+        const job = async () => {
+            try {
+                const token = await getIdTokenExcludingAuth();
+                setSocket(io(
+                    process.env.REACT_APP_API_PREFIX.replaceAll((/^https?/, 'ws')),
+                    {
+                        path: '/api/v1/tenko/session',
+                        extraHeaders: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                ));
+                console.log('Start websocket...');
+            }
+            catch(error) {
+                killSession(null, {
+                    icon: 'error',
+                    title: 'ログイン失敗',
+                    text: error.msg,
+                });
+                await logout();
+            }
+        }
+        job();
+    }, [killSession, setSocket]);
+
+    // 顔の画像を送信
+    useEffect(() => {
+        if (socket !== null) {
+            const interval = setInterval(() => {
+                // 撮影した画像を取得
+                const image = capture();
+                // データ形式の変換
+                const blob = atob(image.replace(/^.*,/, ''));
+                const buffer = new Uint8Array(blob.length);
+                for (let i = 0; i < blob.length; i++) {
+                    buffer[i] = blob.charCodeAt(i);
+                }
+                console.log('顔画像の送信');
+                // 送信
+                socket.emit('faceImage', buffer);
+            }
+            , getCameraSetting().interval);
+            return () => clearInterval(interval);
+        }
+    }, [socket, capture, getCameraSetting]);
+
+
+    const getPhaseLabels = useCallback(() => {
+        return {
+            'FACE_RECOGNITION': '顔認証',
+            '3CHALLENGES': '3チャレンジ',
+        }
+    }, []);
+    const [phase, setPhase] = useReducer((state, arg) => {
+        if (Object.keys(getPhaseLabels()).some(key => key === arg)) {
+            return arg;
+        }
+        else {
+            Swal.fire({
+                icon: 'error',
+                title: 'サーバーエラー',
+                text: '予期しない段階が返ってきました'
+            });
+            return state;
+        }
+    }, null);
+    const getPhaseLabel = useCallback(() => {
+        return getPhaseLabels()[phase];
+    }, [phase, getPhaseLabels])
+
+    const getInstructionMessages = useCallback(() => {
+        return {
+            'FACE_DIRECTION_UP': '上を向いてください',
+            'FACE_DIRECTION_DOWN': '下を向いてください',
+            'FACE_DIRECTION_LEFT': '左を向いてください',
+            'FACE_DIRECTION_RIGHT': '右を向いてください',
+            'FACE_DIRECTION_FRONT': '正面を向いてください',
+            'FACE_NOT_DETECTED': '顔を認識できません',
+            'WAIT': 'そのままお待ち下さい',
+        }
+    }, []);
+    const [instruction, setInstruction] = useReducer((state, arg) => {
+        if (Object.keys(getInstructionMessages()).some(key => key === arg)) {
+            return arg;
+        }
+        else {
+            Swal.fire({
+                icon: 'error',
+                title: 'サーバーエラー',
+                text: '予期しない指示が返ってきました'
+            });
+            return state;
+        }
+    }, null);
+    const getInstructionMessage = useCallback(() => {
+        return getInstructionMessages()[instruction];
+    }, [instruction, getInstructionMessages]);
+
+    const [currentStep, setCurrentStep] = useState(null);
+    const [totalStep, setTotalStep] = useState(null);
+    useEffect(() => {
+        if (socket !== null) {
+            socket.on('instructions', jsonData => {
+                const data = JSON.parse(jsonData);
+
+                setPhase(data.phase);
+                setInstruction(data.instruction);
+
+                switch(data.phase) {
+                    case 'FACE_RECOGNITION':
+                        setCurrentStep(null);
+                        setTotalStep(null);
+                        break;
+                    case '3CHALLENGES':
+                        setCurrentStep(data.steps.current);
+                        setTotalStep(data.steps.total);
+                        break;
+                    default:
+                        killSession(socket, {
+                            icon: 'error',
+                            title: 'サーバーエラー',
+                            text: '取得した現在の段階が正しくありません'
+                        });
+                        break;
+                }
+            });
+        }
+    }, [socket, killSession]);
+    useEffect(() => {
+        if (instruction !== null) {
+            new Audio(
+                `${process.env.PUBLIC_URL}/audio/instruction/${instruction}.mp3`
+            ).play();
+        }
+    }, [instruction]);
+
+    useEffect(() => {
+        if (socket !== null) {
+            socket.io.on("error", (error) => {
+                // ※エラーアラートが表示され続けることを防止する緊急対策
+                if (socket.connected) {
+                    killSession(socket, {
+                        icon: 'error',
+                        title: '点呼のセッションが遮断されました',
+                        text: '予期しないエラー',
+                    });
+                }
+            });
+            socket.on("disconnect", () => {
+                console.log('Kill websocket...');
+                killSession(socket, null);
+            });
+            socket.on('disconnectReason', data => {
+                const job = async () => {
+                    switch(data) {
+                        case 'DONE':
+                            killSession(socket, {
+                                icon: 'warning',
+                                title: '点呼はできません',
+                                text: '点呼はすでに完了しています',
+                            });
+                            break;
+
+                        case 'UNAVAILABLE':
+                            reflectStatus();
+                            killSession(socket, {
+                                icon: 'warning',
+                                title: '点呼はできません',
+                                text: '現在は点呼が実施されていません',
+                            });
+                            break;
+
+                        case 'SUCCESS':
+                            reflectStatus();
+                            killSession(socket, {
+                                icon: 'success',
+                                title: '点呼完了！',
+                                text: '点呼が完了しました',
+                            });
+                            break;
+
+                        case 'INVALID_TOKEN':
+                            killSession(socket, {
+                                icon: 'error',
+                                title: 'サーバーエラー',
+                                text: `無効なトークンです`
+                            });
+                            await logout();
+                            break;
+
+                        case 'INVALID_SESSION':
+                            killSession(socket, {
+                                icon: 'error',
+                                title: '点呼の失敗',
+                                text: `${getPhaseLabel()}が失敗しました`
+                            });
+                            break;
+
+                        default:
+                            killSession(socket, {
+                                icon: 'error',
+                                title: 'サーバーエラー',
+                                text: `取得した接続の遮断理由が正しくありません`
+                            });
+                            break;
+                    }
+                }
+                job();
+            });
+        }
+    }, [socket, getPhaseLabel, killSession, reflectStatus]);
+
+    return (
+        <>
+            {instruction === null ?
+                <div className="flex items-center justify-center pt-12">
+                    <div className="text-gray-600 text-2xl">Loading...</div>
+                </div>
+                :
+                <div className="py-10 md:py-16 px-6 md:px-16 flex flex-col items-center gap-y-3 md:gap-y-12">
+                    <div className="flex flex-col items-center gap-y-2 md:gap-y-5">
+                        {messageHTML}
+                        <div className="text-sm md:text-xl flex">
+                            {Object.keys(getPhaseLabels()).map((key, index) => {
+                                const textColorClassName = (key === phase) ? 'text-white' : 'text-gray-400';
+                                const bgColorClassName = (key === phase) ? 'bg-gray-500' : 'bg-gray-100';
+                                return (
+                                    <div key={key} className={`w-36 md:w-48 ${bgColorClassName} ${textColorClassName} ring-1 ring-gray-300 tracking-wider px-4 py-2 flex gap-x-2`}>
+                                        <div>{`(${index+1})`}</div>
+                                        <div>{getPhaseLabels()[key]}</div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <div className="flex flex-col gap-y-1 md:gap-y-4 text-lg md:text-2xl text-gray-600 tracking-wider">
+                            <div>
+                                {getInstructionMessage()}
+                            </div>
+                            {phase === '3CHALLENGES' &&
+                                <div>{`(${currentStep + 1}/${totalStep})`}</div>
+                            }
+                        </div>
+                    </div>
+                    <Webcam
+                        audio={false}
+                        width={getCameraSetting().width}
+                        height={getCameraSetting().height}
+                        ref={webcamRef}
+                        screenshotFormat={`image/${getCameraSetting().format}`}
+                        videoConstraints={getVideoConstraints()}
+                        mirrored={true}
+                        className="ring-2 ring-gray-500 shadow-lg shadow-gray-400"
+                    ></Webcam>
+                </div>
+            }
+        </>
+    );
+});
+
+const Tenko = React.memo(() => {
     // 点呼ステータスの処理
     const getStatusMessages = useCallback(() => {
         return {
@@ -48,8 +320,9 @@ const RollCall = () => {
         return getStatusMessages()[status]
     }, [status, getStatusMessages]);
 
-    let reflectStatusClk = false;
-    // const reflectStatus = () => reflectStatusClk = !reflectStatusClk;
+    const [reflectStatusClk, reflectStatus] = useReducer((state, action) => {
+        return !state;
+    }, false);
 
     useEffect(() => {
         const job = async () => {
@@ -122,84 +395,49 @@ const RollCall = () => {
         }
     }, [session, dispatchSession]);
 
-
-    // const [phase, setPhase] = useState();
-    // const phaseLabels = {
-    //     'FACE_RECOGNITION': '顔認証',
-    //     '3CHALLENGES': '3チャレンジ',
-    // }
-    // const [instruction, setInstruction] = useState();
-    // const instructionMessages = {
-    //     'FACE_DIRECTION_UP': '上を向いてください',
-    //     'FACE_DIRECTION_DOWN': '上を向いてください',
-    //     'FACE_DIRECTION_LEFT': '上を向いてください',
-    //     'FACE_DIRECTION_RIGHT': '上を向いてください',
-    //     'FACE_DIRECTION_FRONT': '上を向いてください',
-    //     'FACE_NOT_DETECTED': '顔を認識できません',
-    // }
-    // const [currentStep, setCurrentStep] = useState(null);
-    // const [totalStep, setTotalStep] = useState(null);
-    // socket.receive('instructions', jsonData => {
-    //     const data = JSON.parse(jsonData);
-
-    //     switch(data.phase) {
-    //         case 'FACE_RECOGNITION':
-    //             setCurrentStep(null);
-    //             setTotalStep(null);
-    //             break;
-    //         case '3CHALLENGES':
-    //             setCurrentStep(data.steps.current);
-    //             setTotalStep(data.steps.total);
-    //             break;
-    //         default:
-    //             // エラーメッセージ
-    //             break;
-    //     }
-    // });
-
-    // socket.receive('disconnectReason', data => {
-    //     switch(data) {
-    //         case 'DONE':
-    //         case 'UNAVAILABLE':
-    //         case 'SUCCESS':
-    //             reflectStatus();
-    //             break;
-    //         case 'INVALID_TOKEN':
-    //             // エラーメッセージ
-    //             break;
-    //         case 'INVALID_SESSION':
-    //             // エラーメッセージString(
-    //             break;
-    //         default:
-    //             // エラーメッセージ
-    //             break;
-    //     }
-    // });
+    const getMessageHTML = useCallback(() => {
+        return (
+            <div className="flex flex-col md:flex-row gap-y-1 md:gap-y-0 md:gap-x-8">
+                {status !== null &&
+                    <div className="text-xl md:text-3xl text-gray-600 tracking-wider">{getStatusMessage()}</div>
+                }
+                {durationMessage !== null &&
+                    <div className="text-lg md:text-2xl text-gray-400">{`(実施時間：${durationMessage})`}</div>
+                }
+            </div>
+        );
+    }, [status, durationMessage, getStatusMessage]);
 
     return (
         <PageBase
             authType={AUTH_TYPE.AUTH}
             backgroundClassName='bg-white'
-            inner={
-                <div className="py-16 flex flex-col items-center">
-                    <div>
-                        {statusMessages[status]}
-                        （{durationMessage}）
-                        {/* {phaseLabels[phase]} */}
-                        {/* {instructionMessages[instruction]} */}
-                        {/* {phase === '3CHALLENGES' &&
-                            <div>{`${currentStep}/${totalStep}`}</div>
-                        } */}
-                    </div>
-                    {checkCanDo() &&
-                            <Webcam
-                                audio={false}
-                                width={cameraSetting.width}
-                                height={cameraSetting.height}
-                                ref={webcamRef}
-                                screenshotFormat={`image/${cameraSetting.format}`}
-                                videoConstraints={videoConstraints}
-                            ></Webcam>
+            innerHTML={(status === null || durationMessage === null) ?
+                <div className="flex items-center justify-center pt-12">
+                    <div className="text-gray-600 text-2xl">Loading...</div>
+                </div>
+                :
+                <>
+                    {session ?
+                        <TenkoSession
+                            killSession={killSession}
+                            reflectStatus={reflectStatus}
+                            messageHTML={getMessageHTML()}
+                        />
+                        :
+                        <div className="py-10 md:py-16 px-6 md:px-16 flex flex-col items-center gap-y-3 md:gap-y-12">
+                            <div className="flex flex-col items-center gap-y-5">
+                                {getMessageHTML()}
+                            </div>
+                            {checkCanDo() &&
+                                <button
+                                    onClick={() => dispatchSession('start')}
+                                    className="text-md md:text-xl px-3 py-1 md:px-4 md:py-2 rounded-full bg-gray-500 text-white tracking-wider hover:opacity-70"
+                                >
+                                    点呼を実施する
+                                </button>
+                            }
+                        </div>
                     }
                 </>
             }
